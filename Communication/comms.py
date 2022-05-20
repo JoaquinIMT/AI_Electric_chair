672b1393614e61598f3304945fba34f597533539
JoaquinIMT <joaquinla07@gmail.com>
2022-05-19 20:33:14 -0500

    cambios en la forma de hacer modelo, y resultados de analisis facial

--- Communication/comms.py
+++ Communication/comms.py
@@ -6,7 +6,7 @@ class HttpServer():
         self.host_ip = "127.0.0.1"
         self.port = 65432
 
-    def open_comm(self):
+    def open_comm(self):        
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         self.s.bind((self.host_ip,self.port))
         self.s.listen()
--- Communication/t.py
+++ Communication/t.py
@@ -1,29 +1,26 @@
-#source = https://www.tutorialspoint.com/python/python_multithreading.htm
+
 import threading
 import time
 
 exitFlag = 0
 
-##STILL NOT FINISHED
 class myThread (threading.Thread):
-   
-   def __init__(self, threadID, name, increment):
+   def __init__(self, threadID, name, counter):
       threading.Thread.__init__(self)
       self.threadID = threadID
       self.name = name
-      self.increment = increment
-
+      self.counter = counter
    def run(self):
-      print("Starting " + self.name)
-      counter(self.increment)
-      print("Exiting " + self.name)
+      print ("Starting " + self.name)
+      print_time(self.name, 5, self.counter)
+      print ("Exiting " + self.name)
 
-def counter(threadName, counter, delay):
+def print_time(threadName, counter, delay):
    while counter:
       if exitFlag:
          threadName.exit()
       time.sleep(delay)
-      print(count)
+      print ("%s: %s" % (threadName, time.ctime(time.time())))
       counter -= 1
 
 # Create new threads
@@ -34,4 +31,4 @@ thread2 = myThread(2, "Thread-2", 2)
 thread1.start()
 thread2.start()
 
-print("Exiting Main Thread")
+print ("Exiting Main Thread")
